1fabb747ecc7c2202b594687ad12b6fe9bce04f6
Ivan Vakhrushev <mfvanek@gmail.com>
2020-07-29 18:58:10 +0300

    Updated dependencies

--- src/main/java/io/github/mfvanek/spring5mvc/AppConfig.java
+++ src/main/java/io/github/mfvanek/spring5mvc/AppConfig.java
@@ -16,23 +16,23 @@ import javax.servlet.ServletRegistration;
 @ComponentScan(basePackages = {"io.github.mfvanek.spring5mvc"})
 public class AppConfig implements WebApplicationInitializer {
 
-	@Override
-	public void onStartup(ServletContext container) {
-		// Create the 'root' Spring application context
-		AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
-		rootContext.register(AppConfig.class);
+    @Override
+    public void onStartup(ServletContext container) {
+        // Create the 'root' Spring application context
+        AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
+        rootContext.register(AppConfig.class);
 
-		// Manage the lifecycle of the root application context
-		container.addListener(new ContextLoaderListener(rootContext));
+        // Manage the lifecycle of the root application context
+        container.addListener(new ContextLoaderListener(rootContext));
 
-		// Create the dispatcher servlet's Spring application context
-		AnnotationConfigWebApplicationContext dispatcherContext = new AnnotationConfigWebApplicationContext();
+        // Create the dispatcher servlet's Spring application context
+        AnnotationConfigWebApplicationContext dispatcherContext = new AnnotationConfigWebApplicationContext();
 
-		// Register and map the dispatcher servlet
-		ServletRegistration.Dynamic dispatcher = container
-				.addServlet("dispatcher", new DispatcherServlet(dispatcherContext));
-		dispatcher.setLoadOnStartup(1);
-		dispatcher.addMapping("/");
-	}
+        // Register and map the dispatcher servlet
+        ServletRegistration.Dynamic dispatcher = container
+                .addServlet("dispatcher", new DispatcherServlet(dispatcherContext));
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/");
+    }
 
 }
--- src/main/java/io/github/mfvanek/spring5mvc/Main.java
+++ src/main/java/io/github/mfvanek/spring5mvc/Main.java
@@ -6,40 +6,41 @@ import java.io.File;
 import java.io.IOException;
 
 public class Main {
-	private static final int PORT = getPort();
 
-	public static void main(String[] args) throws Exception {
-		String appBase = ".";
-		Tomcat tomcat = new Tomcat();
-		tomcat.setBaseDir(createTempDir());
-		tomcat.setPort(PORT);
-		tomcat.getHost().setAppBase(appBase);
-		tomcat.addWebapp("", ".");
-		tomcat.start();
-		tomcat.getServer().await();
-	}
+    private static final int PORT = getPort();
 
-	private static int getPort() {
-		String port = System.getenv("PORT");
-		if (port != null) {
-			return Integer.valueOf(port);
-		}
-		return 8080;
-	}
+    public static void main(String[] args) throws Exception {
+        String appBase = ".";
+        Tomcat tomcat = new Tomcat();
+        tomcat.setBaseDir(createTempDir());
+        tomcat.setPort(PORT);
+        tomcat.getHost().setAppBase(appBase);
+        tomcat.addWebapp("", ".");
+        tomcat.start();
+        tomcat.getServer().await();
+    }
 
-	// based on AbstractEmbeddedServletContainerFactory
-	private static String createTempDir() {
-		try {
-			File tempDir = File.createTempFile("tomcat.", "." + PORT);
-			tempDir.delete();
-			tempDir.mkdir();
-			tempDir.deleteOnExit();
-			return tempDir.getAbsolutePath();
-		} catch (IOException ex) {
-			throw new RuntimeException(
-					"Unable to create tempDir. java.io.tmpdir is set to " + System.getProperty("java.io.tmpdir"),
-					ex
-			);
-		}
-	}
+    private static int getPort() {
+        String port = System.getenv("PORT");
+        if (port != null) {
+            return Integer.valueOf(port);
+        }
+        return 8080;
+    }
+
+    // based on AbstractEmbeddedServletContainerFactory
+    private static String createTempDir() {
+        try {
+            File tempDir = File.createTempFile("tomcat.", "." + PORT);
+            tempDir.delete();
+            tempDir.mkdir();
+            tempDir.deleteOnExit();
+            return tempDir.getAbsolutePath();
+        } catch (IOException ex) {
+            throw new RuntimeException(
+                    "Unable to create tempDir. java.io.tmpdir is set to " + System.getProperty("java.io.tmpdir"),
+                    ex
+            );
+        }
+    }
 }
