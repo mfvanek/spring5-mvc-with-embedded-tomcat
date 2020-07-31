19a8e8d32c281bf7aed9827b0fa3d4acc8412942
Ivan Vakhrushev <IVakhrushev@innotechnum.com>
2020-07-31 12:59:15 +0300

    Added UdpSender

--- src/main/java/io/github/mfvanek/spring5mvc/config/TracingConfig.java
+++ src/main/java/io/github/mfvanek/spring5mvc/config/TracingConfig.java
@@ -6,6 +6,8 @@ import io.jaegertracing.internal.reporters.CompositeReporter;
 import io.jaegertracing.internal.reporters.LoggingReporter;
 import io.jaegertracing.internal.reporters.RemoteReporter;
 import io.jaegertracing.internal.samplers.ConstSampler;
+import io.jaegertracing.spi.Reporter;
+import io.jaegertracing.thrift.internal.senders.UdpSender;
 import io.opentracing.Tracer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,11 +24,21 @@ public class TracingConfig {
     }
 
     @Bean
-    public Tracer tracer(@Nonnull MDCScopeManager mdcScopeManager) {
+    public Reporter reporter() {
+        return new CompositeReporter(
+                new LoggingReporter(),
+                new RemoteReporter.Builder()
+                        .withSender(new UdpSender())
+                        .build()
+        );
+    }
+
+    @Bean
+    public Tracer tracer(@Nonnull MDCScopeManager mdcScopeManager, @Nonnull Reporter reporter) {
         return new JaegerTracer.Builder("spring5-mvc-with-embedded-tomcat")
                 .withSampler(new ConstSampler(true))
                 .withScopeManager(mdcScopeManager)
-                .withReporter(new CompositeReporter(new LoggingReporter(), new RemoteReporter.Builder().build()))
+                .withReporter(reporter)
                 .build();
     }
 }
