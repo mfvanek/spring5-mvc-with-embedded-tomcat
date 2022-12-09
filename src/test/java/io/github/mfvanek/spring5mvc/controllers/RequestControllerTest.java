548d10e9f79b144c43bf286bf14d1d600c4a963b
Ivan Vakhrushev <i.vakhrushev@banki.ru>
2022-12-09 19:15:33 +0300

    Switched to Java 17

--- src/test/java/io/github/mfvanek/spring5mvc/controllers/RequestControllerTest.java
+++ src/test/java/io/github/mfvanek/spring5mvc/controllers/RequestControllerTest.java
@@ -1,13 +1,13 @@
 package io.github.mfvanek.spring5mvc.controllers;
 
 import io.github.mfvanek.spring5mvc.config.WebConfig;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -16,64 +16,67 @@ import org.springframework.web.context.WebApplicationContext;
 
 import javax.servlet.ServletContext;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.instanceOf;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.startsWith;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = {WebConfig.class})
 @WebAppConfiguration
-public class RequestControllerTest {
+class RequestControllerTest {
 
     @Autowired
     private WebApplicationContext wac;
 
     private MockMvc mockMvc;
 
-    @Before
-    public void setup() {
+    @BeforeEach
+    void setup() {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
     }
 
     @Test
-    public void verifyTestConfiguration() {
-        ServletContext servletContext = wac.getServletContext();
+    void verifyTestConfiguration() {
+        final ServletContext servletContext = wac.getServletContext();
 
-        assertNotNull(servletContext);
-        assertThat(servletContext, instanceOf(MockServletContext.class));
-        assertNotNull(wac.getBean("requestController"));
+        assertThat(servletContext)
+                .isNotNull()
+                .isInstanceOf(MockServletContext.class);
+        assertThat(wac.getBean("requestController"))
+                .isNotNull()
+                .isInstanceOf(RequestController.class);
     }
 
     @Test
-    public void log() throws Exception {
+    void log() throws Exception {
         MvcResult mvcResult = this.mockMvc.perform(get("/api/v1/demo"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andReturn();
 
-        assertEquals("text/plain;charset=ISO-8859-1", mvcResult.getResponse().getContentType());
-        assertThat(mvcResult.getResponse().getContentAsString(), startsWith("Add log entry at "));
+        assertThat(mvcResult.getResponse().getContentType())
+                .isEqualTo("text/plain;charset=ISO-8859-1");
+        assertThat(mvcResult.getResponse().getContentAsString())
+                .startsWith("Add log entry at ");
     }
 
     @Test
-    public void logException() throws Exception {
+    void logException() throws Exception {
         MvcResult mvcResult = this.mockMvc.perform(get("/api/v1/demoException"))
                 .andDo(print())
                 .andExpect(status().is5xxServerError())
                 .andExpect(jsonPath("$.message").value("sample exception"))
                 .andReturn();
 
-        assertEquals("application/json", mvcResult.getResponse().getContentType());
+        assertThat(mvcResult.getResponse().getContentType())
+                .isEqualTo("application/json");
     }
 
     @Test
-    public void displayWelcomeMessage() throws Exception {
+    void displayWelcomeMessage() throws Exception {
         MvcResult mvcResult = this.mockMvc.perform(get("/api/v1/welcome"))
                 .andDo(print())
                 .andExpect(status().isOk())
@@ -81,11 +84,12 @@ public class RequestControllerTest {
                 .andExpect(jsonPath("description").value(startsWith("Welcome from Spring MVC and Embedded Tomcat. ")))
                 .andReturn();
 
-        assertEquals("application/json", mvcResult.getResponse().getContentType());
+        assertThat(mvcResult.getResponse().getContentType())
+                .isEqualTo("application/json");
     }
 
     @Test
-    public void apiDocsShouldWork() throws Exception {
+    void apiDocsShouldWork() throws Exception {
         MvcResult mvcResult = this.mockMvc.perform(get("/v2/api-docs"))
                 .andDo(print())
                 .andExpect(status().isOk())
@@ -93,6 +97,7 @@ public class RequestControllerTest {
                 .andExpect(jsonPath("$.definitions").exists())
                 .andReturn();
 
-        assertEquals("application/json", mvcResult.getResponse().getContentType());
+        assertThat(mvcResult.getResponse().getContentType())
+                .isEqualTo("application/json");
     }
 }
